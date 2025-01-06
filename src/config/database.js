1f2dbc40ae9dbcb158716266980c5bbd1e62d392
Disha Hati <dishahati55@gmail.com>
2025-01-06 21:45:03 +0530

    Corrected Constants

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose');
 
 const connectDB=async()=>{
-    await mongoose.connect(
-        process.env.MONGO_CONNECTION_URL
-    )
+    console.log(process.env.MONGO_CONNECTION_URL);
+    await mongoose.connect(process.env.MONGO_CONNECTION_URL);
+    
 };
 
 module.exports=connectDB;
